0b631df06139c486295b3a38324aa49b56c678b6
livtecq <nishiki1202@gmail.com>
2021-11-27 18:13:41 +0900

    postSliceの作成

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,13 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
+import postReducer from "../features/post/postSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     auth: authReducer,
+    post: postReducer,
   },
 });
-
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<
